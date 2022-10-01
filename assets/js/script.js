ab87be2d7249ca368f4c6131a06a232023119fa6
Ilman Qori <ilmanqori@Ilmans-iMac.local>
2022-10-02 00:58:42 +0700

    update logo

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,8 +26,8 @@ function showTime() {
   m = m < 10 ? "0" + m : m;
   s = s < 10 ? "0" + s : s;
 
-  //   var time = h + ":" + m + ":" + s + " " + session;
-  var time = h + ":" + m;
+  // var time = h + ":" + m + ":" + s + " " + session;
+  var time = h + ":" + m + ":" + s;
   document.getElementById("MyClockDisplay").innerText = time;
   document.getElementById("MyClockDisplay").textContent = time;
 
